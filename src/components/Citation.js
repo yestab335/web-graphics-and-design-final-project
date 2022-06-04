41c88c80b123f67dfbd73ebecc8a8a80e0b02e5f
Rayan Tabora <superguy723@Rayans-MacBook-Pro.local>
2022-06-03 23:57:53 -0400

    React App Components

---
+++ src/components/Citation.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import './PricingStyles.css'
+
+const Pricing = () => {
+    return (
+        <div className='pricing'>
+            <div className='card-container'>
+                <div className='card'>
+                    <h3>- Favicon -</h3>
+                    <span className='bar'></span>
+                    <p>- pokpak -</p>
+                    <p>- vecteezy -</p>
+                    <p>- Pro License -</p>
+                    <a href='https://bit.ly/3wQrkYm' className='btn' target='_blank'>View Source</a>
+                </div>
+                <div className='card'>
+                    <h3>- Background Video -</h3>
+                    <span className='bar'></span>
+                    <p>- Space Space -</p>
+                    <p>- pexels -</p>
+                    <p>- Free to Use License -</p>
+                    <a href='https://bit.ly/38LQjmr' className='btn' target='_blank'>View Source</a>
+                </div>
+                <div className='card'>
+                    <h3>- Hero Image -</h3>
+                    <span className='bar'></span>
+                    <p>- N/A -</p>
+                    <p>- edupress -</p>
+                    <p>- No License -</p>
+                    <a href='https://bit.ly/3NBRNhD' className='btn' target='_blank'>View Source</a>
+                </div>
+                <div className='card'>
+                    <h3>- Astronaut -</h3>
+                    <span className='bar'></span>
+                    <p>- Pixabay -</p>
+                    <p>- pexels -</p>
+                    <p>- Free to Use License -</p>
+                    <a href='https://bit.ly/39WSXWo' className='btn' target='_blank'>View Source</a>
+                </div>
+                <div className='card'>
+                    <h3>- Space Pod -</h3>
+                    <span className='bar'></span>
+                    <p>- Evan Buchholz -</p>
+                    <p>- pexels -</p>
+                    <p>- Free to Use License -</p>
+                    <a href='https://bit.ly/3wP3uMs' className='btn' target='_blank'>View Source</a>
+                </div>
+                <div className='card'>
+                    <h3>- 404 Page -</h3>
+                    <span className='bar'></span>
+                    <p>- Pixabay -</p>
+                    <p>- pexels -</p>
+                    <p>- Free to Use License -</p>
+                    <a href='https://bit.ly/3sZ38jQ' className='btn' target='_blank'>View Source</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Pricing
